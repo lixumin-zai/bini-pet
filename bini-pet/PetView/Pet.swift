d25cb522833a4fd968c43fd4a493d9d26e47357d
lismin <lixumin1030@gmail.com>
2025-01-27 17:49:11 +0800

    feat: 添加玩具

--- bini-pet/PetView/Pet.swift
+++ bini-pet/PetView/Pet.swift
@@ -34,13 +34,12 @@ enum WalkingStatus {
 
 struct Pet: View {
     
+    @Binding var status: Status
+    @Binding var isMovingRight: Bool
+    @Binding var pettingTimes: Int  // 宠物的抚摸步
+    
     @State private var offsetX: CGFloat = 0
     @State private var offsetY: CGFloat = 0
-    
-    @Binding var status: Status
-//    @Binding var walkingStatus: WalkingStatus
-    
-    @Binding var isMovingRight: Bool
     @State private var isMovingUp: Bool = true // 控制移动方向
     @State private var imageUrl: URL? = Bundle.main.url(forResource: "brown_idle_8fps", withExtension: "gif")
     private let HorizontalStepSize: CGFloat = 7 // 每次移动的步长
@@ -50,24 +49,38 @@ struct Pet: View {
     
     var body: some View {
         WebImage(url: imageUrl)
-        .resizable()
-        .scaledToFit()
-        .frame(width: 40, height: 40)
-        .overlay(
-            RoundedRectangle(cornerRadius: 20)
-                .stroke(Color.blue, lineWidth: 2) // 设置边框颜色和宽度
-        )
-        .scaleEffect(x: isMovingRight ? 1 : -1, y: 1) // 水平翻转
-        .offset(x: offsetX, y: offsetY)
-        .onChange(of: status) { newValue in
-            imageUrl = getImageUrl() // 当 isStop 变化时更新图片 URL
-        }
+            .resizable()
+            .scaledToFit()
+            .frame(width: 40, height: 40)
+            .overlay(
+                RoundedRectangle(cornerRadius: 20)
+                    .stroke(Color.blue, lineWidth: 2) // 设置边框颜色和宽度
+            )
+            .scaleEffect(x: isMovingRight ? 1 : -1, y: 1) // 水平翻转
+            .offset(x: offsetX, y: offsetY)
+            .contentShape(Rectangle().size(width: 20, height: 20)) // 限制点击区域为 20x20
+            .onChange(of: status) { newValue in
+                imageUrl = getImageUrl() // 当 isStop 变化时更新图片 URL
+            }
+            .gesture(
+                DragGesture(minimumDistance: 0)
+                    .onChanged { value in
+                    // 获取手指当前位置
+//                    self.tapLocation = value.location
+                        pettingTimes = 5
+                        status = .petting
+                    }
+            )
     }
     
     // 动态选择图片 URL
     func getImageUrl() -> URL? {
         if status == .standing {
             return Bundle.main.url(forResource: "brown_idle_8fps", withExtension: "gif")
+        } else if status == .eating {
+            return Bundle.main.url(forResource: "brown_idle_8fps", withExtension: "gif")
+        } else if status == .petting {
+            return Bundle.main.url(forResource: "brown_petting_8fps", withExtension: "gif")
         } else {
             return Bundle.main.url(forResource: "brown_walk_fast_8fps", withExtension: "gif")
         }
--- bini-pet/PetView/Sence.swift
+++ bini-pet/PetView/Sence.swift
@@ -23,83 +23,115 @@ struct Sence: View {
     @State private var VerticalStepSize: CGFloat = 7 // 每次移动的步长
     
     @State private var petStatus: Status = .standing  // 宠物的状态
+    @State private var pettingTimes: Int = 0  // 宠物的抚摸步
     
     @State private var hunger: Int = 50  // 饱腹值
     @State private var bowlPosition: CGPoint = CGPoint(x: 40, y: 140)
     
     var body: some View {
         ZStack {
-//            WebImage(url: Bundle.main.url(forResource: "room", withExtension: "png"))
-//                .resizable()
-//                .scaledToFit()
-//                .frame(width: 200, height: 200)
-            Pet(status: $petStatus, isMovingRight: $isMovingRight)
-                .position(x: petOriginPosition.x, y: petOriginPosition.y)
-                .offset(x: petPosition.x, y: petPosition.y) // 根据 petPosition 移动 Pet()
-                .background(GeometryReader { geometry in
-                    Color.clear
-                        .onAppear {
-                            // 获取 Pet() 的尺寸
-                            self.petSize = geometry.size
-                            print(geometry.size)
-                        }
-                })
-                .onAppear {
-//                    petStatus = .standing
-//                    stopWalk()
-                    startStepMovement()
+            
+            
+            ZStack {
+                // 背景
+                WebImage(url: Bundle.main.url(forResource: "room", withExtension: "png"))
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+                // 窗户背景
+                WebImage(url: Bundle.main.url(forResource: "outside_base", withExtension: "jpeg"))
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 60, height: 60)
+                    .position(x: 80, y: 60)
+                if tapLocation != .zero {
+                    Circle()
+                        .fill(Color.green)
+                        .frame(width: 6, height: 6)
+                        .position(x: tapLocation.x, y: tapLocation.y)
+                        .drawingGroup() // 渲染为位图，避免抗锯齿
                 }
-        }
-        .frame(width: 200, height: 200)
-        .clipped()
-        .background(Color.gray)
-        .contentShape(Rectangle()) // 确保整个区域都可以响应点击
-        .gesture(
-            DragGesture(minimumDistance: 0)
-                .onChanged { value in
-                // 获取手指当前位置
-                self.tapLocation = value.location
+                Pet(status: $petStatus, isMovingRight: $isMovingRight, pettingTimes: $pettingTimes)
+                    .position(x: petOriginPosition.x, y: petOriginPosition.y)
+                    .offset(x: petPosition.x, y: petPosition.y) // 根据 petPosition 移动 Pet()
+                    .background(GeometryReader { geometry in
+                        Color.clear
+                            .onAppear {
+                                // 获取 Pet() 的尺寸
+                                self.petSize = geometry.size
+                                print(geometry.size)
+                            }
+                    })
+                    .onAppear {
+                        //                    petStatus = .standing
+                        //                    stopWalk()
+                        startStepMovement()
+                    }
                 
-                // 计算 pet 的当前位置
-                let petCenter = CGPoint(x: self.petPosition.x + self.petOriginPosition.x,
-                                        y: self.petPosition.y + self.petOriginPosition.y)
-                
-                // 计算 tapLocation 和 petCenter 之间的向量
-                let deltaX = self.tapLocation.x - petCenter.x
-                let deltaY = self.tapLocation.y - petCenter.y
-                
-                // 计算距离
-                let distance = sqrt(deltaX * deltaX + deltaY * deltaY)
-                
-
-                // 更新移动方向
-                self.isMovingRight = deltaX > 0
-                self.isMovingUp = deltaY > 0  // 注意：Y轴方向在屏幕上是从上到下增加的
-                
-                // 根据距离调整步长（可以根据需要调整步长的计算方式）
-                self.HorizontalStepSize = abs(deltaX) / distance * 7
-                self.VerticalStepSize = abs(deltaY) / distance * 7
-                
-                petStatus = .walking
             }
-        )
+            .frame(width: 200, height: 200)
+            .clipped()
+            .contentShape(Rectangle()) // 确保整个区域都可以响应点击
+            .gesture(
+                DragGesture(minimumDistance: 0)
+                    .onChanged { value in
+                        // 获取手指当前位置
+                        self.tapLocation = value.location
+                        
+                        // 计算 pet 的当前位置
+                        let petCenter = CGPoint(x: self.petPosition.x + self.petOriginPosition.x,
+                                                y: self.petPosition.y + self.petOriginPosition.y)
+                        
+                        // 计算 tapLocation 和 petCenter 之间的向量
+                        let deltaX = self.tapLocation.x - petCenter.x
+                        let deltaY = self.tapLocation.y - petCenter.y
+                        
+                        // 计算距离
+                        let distance = sqrt(deltaX * deltaX + deltaY * deltaY)
+                        
+                        
+                        // 更新移动方向
+                        self.isMovingRight = deltaX > 0
+                        self.isMovingUp = deltaY > 0  // 注意：Y轴方向在屏幕上是从上到下增加的
+                        
+                        // 根据距离调整步长（可以根据需要调整步长的计算方式）
+                        self.HorizontalStepSize = abs(deltaX) / distance * 7
+                        self.VerticalStepSize = abs(deltaY) / distance * 7
+                        
+                        petStatus = .walking
+                    }
+                )
+            Text("123")
+                .font(.system(size: 24, weight: .bold)) // 设置字体大小和粗体
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center) // 居中
+        }
+        .background(Color.black)
     }
+    
     func startWalk() {
         petStatus = .walking
         if petPosition.x > 70 {
-            petStatus = .standing
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.47) {
+                petStatus = .standing
+            }
             isMovingRight = false
         }
         if petPosition.x < -32.0 {
-            petStatus = .standing
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.47) {
+                petStatus = .standing
+            }
             isMovingRight = true
         }
         if petPosition.y > 32.0 {
-            petStatus = .standing
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.47) {
+                petStatus = .standing
+            }
             isMovingUp = false
         }
         if petPosition.y < -32.0 {
-            petStatus = .standing
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.47) {
+                petStatus = .standing
+            }
             isMovingUp = true
         }
         let horizontalStep = isMovingRight ? HorizontalStepSize : -HorizontalStepSize
@@ -133,8 +165,20 @@ struct Sence: View {
             
             if petStatus == .walking {
                 startWalk()
-            } else if petStatus == .toEat{
+            } else if petStatus == .toEat {
                 toEat()
+                
+
+            } else if petStatus == .eating {
+                //                DispatchQueue.main.asyncAfter(deadline: .now() + 5) {  // 持续1分钟
+                //                    petStatus = .standing
+                //                    print(petStatus)
+                //                }
+            } else if petStatus == .petting {
+                pettingTimes -= 1
+                if pettingTimes == 0 {
+                    petStatus = .standing
+                }
             } else if Int.random(in: 0...10) > 8 {
                 petStatus = .randomWalking
                 randomStartWalk()
@@ -194,10 +238,6 @@ struct Sence: View {
         // 如果距离小于某个阈值（例如 1），则认为已经到达目标位置
         if distance < 5 {
             petStatus = .eating
-            DispatchQueue.main.asyncAfter(deadline: .now() + 5) {  // 持续1分钟
-                petStatus = .standing
-                print(petStatus)
-            }
         }
         print(bowlPosition)
         print(petPosition.x, petPosition.y)
